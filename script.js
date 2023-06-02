905e4e7961014dbd35b6c1ff1d8026496f951aac
Lukasz Jasionowski <jasionowski.lukasz@gmail.com>
2023-06-02 22:16:34 +0200

    Use DOM and add 'newQuote' function

---
+++ script.js
@@ -0,0 +1,19 @@
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
+
+newQuote = () => {
+    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)]
+    // Check Quote length to determine styling
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;
+    authorText.textContent = quote.author;
+}
+
+newQuote();
