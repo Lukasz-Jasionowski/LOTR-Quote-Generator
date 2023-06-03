9f37603f594b38514495000ca020402b492640fa
Lukasz Jasionowski <jasionowski.lukasz@gmail.com>
2023-06-03 20:25:06 +0200

    Create loading and tweeting functions

--- script.js
+++ script.js
@@ -3,8 +3,22 @@ const quoteText = document.getElementById('quote')
 const authorText = document.getElementById('author')
 const twitterBtn = document.getElementById('twitter')
 const newQuoteBtn = document.getElementById('new-quote')
+const loader = document.getElementById('loader');
+
+// Loading
+loading = () => {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+loadingComplete = () => {
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
 
 newQuote = () => {
+    loading();
     const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)]
     // Check Quote length to determine styling
     if (quote.text.length > 120) {
@@ -14,6 +28,17 @@ newQuote = () => {
     }
     quoteText.textContent = quote.text;
     authorText.textContent = quote.author;
+    loadingComplete();
 }
 
-newQuote();
+// Tweet
+tweetQuote = () => {
+    const twitterURL = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterURL, '_blank');
+}
+
+//Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+newQuote(); 
